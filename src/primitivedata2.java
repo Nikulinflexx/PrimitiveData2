215aa6be57a9082ea98bcc272893e71628d6b0cb
Nikita <nickita.nikulin2015@yandex.ru>
2024-08-09 23:16:13 +0300

    Initial commit

---
+++ src/primitivedata2.java
@@ -0,0 +1,17 @@
+package src;
+
+public class primitivedata2 {
+    public static void main(String[] args) {
+        int threshold = 1000;
+        int accountsRefill;
+        int bonus;
+        accountsRefill = 1100;
+        if(accountsRefill > threshold) {
+            bonus = accountsRefill / 100;
+        } else {
+            bonus = 0;
+        }
+
+        System.out.println(bonus);
+    }
+}
